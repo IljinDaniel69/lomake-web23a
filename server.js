82a633a208d7d1ad4227cc591aa6f094d9cb023c
Joonas Iivonen <joonas.iivonen@gmail.com>
2025-01-23 09:26:59 +0200

    Feedback form handling

--- server.js
+++ server.js
@@ -15,9 +15,21 @@ app.set('views', path.join(__dirname, 'templates'));
 
 app.use('/styles', express.static('includes/styles'));
 
+app.use(express.urlencoded({ extended: true }));
+
 // TODO: Tähän tulevat polkumäärittelyt
 app.get('/', (req, res) => {
     res.render('index');
 });
+app.get('/palaute', (req, res) => {
+    res.render('palaute');
+});
+app.post('/palaute', (req, res) => {
+    let name = req.body.name;
+    let email = req.body.email;
+
+    res.render('vastaus', { name: name, email: email });
+});
+
 
 app.listen(port, host, () => console.log(`${host}:${port} kuuntelee...`));
